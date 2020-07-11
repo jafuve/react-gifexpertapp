6e5cef830fb8f3006859f37e8f145f171e2f9661
Jairo Fuentes <jairo_fuentesv@hotmail.com>
2020-07-10 18:24:20 -0600

    docs agregado

---
+++ docs/precache-manifest.7377ac57612a4d2c9c5b0a2b5ce84fbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a247fa3f9d06dc4e5364a5b5e678acd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4390deee3d5a8389255c",
+    "url": "/static/css/main.ad98072a.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4390deee3d5a8389255c",
+    "url": "/static/js/main.55bd70d5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
